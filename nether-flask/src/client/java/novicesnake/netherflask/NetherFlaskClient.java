59f672b98ebaf14017648189f7f1d698a7f5ad02
P. Kail <pkail@uni-bremen.de>
2024-09-15 02:20:44 +0200

    Initial Push
    The Nether Flask is supposed to be a rechargeable Flask that stores an potions effect.

---
+++ nether-flask/src/client/java/novicesnake/netherflask/NetherFlaskClient.java
@@ -0,0 +1,10 @@
+package novicesnake.netherflask;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NetherFlaskClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ nether-flask/src/main/java/novicesnake/netherflask/NetherFlaskDataGenerator.java
@@ -0,0 +1,11 @@
+package novicesnake.netherflask;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NetherFlaskDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
